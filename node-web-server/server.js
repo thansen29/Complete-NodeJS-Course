b70f9c3618b5aa7ce413f54ac72fcd3db4d53ed7
Thomas Hansen <thansen1694@gmail.com>
2018-03-23 14:54:26 -0400

    Handlebars partials and helpers

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,15 +3,22 @@ const hbs = require('hbs');
 
 const app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'What is up my dudes'
   })
 });
@@ -19,7 +26,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About',
-    currentYear: new Date().getFullYear()
   });
 })
 
